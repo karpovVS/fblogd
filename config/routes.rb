eed6b4c682632ca56551a9275cb94d80e1ed8f7b
karpovVS <kazankarpov@gmail.com>
2019-12-13 01:21:49 +0300

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ get 'about', to: 'pages#about'
 
 
 resources :articles
+
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
